a30c7b0d5330c782e71362acf01a3218a5e3834b
Mustapha-AJEGHRIR <m5u9s00@gmail.com>
2022-07-31 21:43:49 +0200

    starting Yet another ...

---
+++ Yet Another Problem About Pairs Satisfying an Inequality/main.py
@@ -0,0 +1,17 @@
+"""
+You are given an array a1,a2,…an. Count the number of pairs of indices 1≤i,j≤n such that ai<i<aj<j.
+
+Input
+The first line contains an integer t (1≤t≤1000) — the number of test cases.
+
+The first line of each test case contains an integer n (2≤n≤2⋅105) — the length of the array.
+
+The second line of each test case contains n integers a1,a2,…,an (0≤ai≤109) — the elements of the array.
+
+It is guaranteed that the sum of n across all test cases does not exceed 2⋅105.
+
+Output
+For each test case, output a single integer — the number of pairs of indices satisfying the condition in the statement.
+
+Please note, that the answer for some test cases won't fit into 32-bit integer type, so you should use at least 64-bit integer type in your programming language (like long long for C++).
+"""
